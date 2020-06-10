634a053d477ed1152855254b8eafaefe0ce9b36d
zlx <3371994757@qq.com>
2020-06-10 16:17:45 +0800

    这个工程提供给用户课程检索,课程分类查询,用户登录，用户登出等功能

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--nuxt.js将/layout/default.vue作为所有页面的默认布局，通常布局包括：页头、内容区、页尾-->
+  <div>
+    <Header />
+    <nuxt/>
+    <Footer />
+  </div>
+</template>
+<script>
+  import Footer from '../components/Footer.vue'
+  import Header from '../components/Header.vue'
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ layouts/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>这里是页面头部</div>
+    <nuxt/>
+    <div>这里是页面尾部</div>
+  </div>
+</template>
+<script>
+  export default{
+
+  }
+</script>
+<style>
+
+</style>
